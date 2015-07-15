be1a04db8c445799cb191f320f1aaced4d3c8a92
Sadhika Malladi <sadhika.malladi@outlook.com>
2015-07-15 14:49:02 -0400

    Added CVM functionality...

--- R/CVM.R
+++ R/CVM.R
@@ -1,22 +1,7 @@
-#' Earth Mover's Distance algorithm for differential analysis of genomics data.
-#'
-#' \code{\link{calculate_emd}} will usually be the only function needed.
-#'
-#'
-#' @import emdist
-#' @import BiocParallel
-#' @import matrixStats
-#' @import ggplot2
-#' @import CDFt
-#' @name emdomics-package
-#' @docType package
-NULL
-
-
 #' @export
 #' @title Earth Mover's Distance for differential analysis of genomics data
 #' @description This is the main user interface to the \pkg{EMDomics} package, and
-#' will usually the only function needed when conducting an analysis using the EMD
+#' will usually the only function needed when conducting an analysis using the CVM
 #' algorithm. Analyses can also be conducted with the Komolgorov-Smirnov Test using
 #' \code{calculate_ks}.
 #'
@@ -32,22 +17,22 @@ NULL
 #' highly heterogeneous, which can be the case in many biological circumstances
 #' (e.g sensitive vs. resistant tumor samples).
 #'
-#' The Earth Mover's Distance algorithm instead computes the "work" needed
-#' to transform one distribution into another, thus capturing possibly
-#' valuable information relating to the overall difference in shape between
-#' two heterogeneous distributions.
+#' The Cramer von Mises (CVM) algorithm generates a test statistic that is the sum of 
+#' the squared values of the differences between two cumulative distribution functions (CDFs). 
+#' As a result, the test statistic tends to overestimate the similarity between two distributions and
+#' cannot effectively handle partial matching like EMD does.
 #'
-#' The EMD-based algorithm implemented in \pkg{EMDomics} has two main steps.
+#' The CVM-based algorithm implemented in \pkg{EMDomics} has two main steps.
 #' First, a matrix (e.g. of expression data) is divided into data for each of the groups.
-#' Every possible pairwise EMD score is then computed and stored in a table. The EMD score
-#' for a single gene is calculated by averaging all of the pairwise EMD scores.
+#' Every possible pairwise CVM score is then computed and stored in a table. The CVM score
+#' for a single gene is calculated by averaging all of the pairwise CVM scores.
 #' Next, the labels for each of the groups are randomly
-#' permuted a specified number of times, and an EMD score for each permutation is
+#' permuted a specified number of times, and an CVM score for each permutation is
 #' calculated. The median of the permuted scores for each gene is used as
 #' the null distribution, and the False Discovery Rate (FDR) is computed for
 #' a range of permissive to restrictive significance thresholds. The threshold
 #' that minimizes the FDR is defined as the q-value, and is used to interpret
-#' the significance of the EMD score analogously to a p-value (e.g. q-value
+#' the significance of the CVM score analogously to a p-value (e.g. q-value
 #' < 0.05 = significant.)
 #'
 #' @param data A matrix containing genomics data (e.g. gene expression levels).
@@ -55,15 +40,13 @@ NULL
 #' contain sample identifiers.
 #' @param outcomes A vector containing group labels for each of the samples provided
 #' in the \code{data} matrix. The names should be the sample identifiers provided in \code{data}.
-#' @param binSize The bin size to be used when generating histograms of
-#' the data for each group. Defaults to 0.2.
-#' @param nperm An integer specifying the number of randomly permuted EMD
+#' @param nperm An integer specifying the number of randomly permuted CVM
 #' scores to be computed. Defaults to 100.
 #' @param verbose Boolean specifying whether to display progress messages.
 #' @param parallel Boolean specifying whether to use parallel processing via
 #' the \pkg{BiocParallel} package. Defaults to \code{TRUE}.
-#' @return The function returns an \code{\link{EMDomics}} object.
-#' @examples
+#' @return The function returns an \code{\link{CVMomics}} object.
+#' examples
 #' # 100 genes, 100 samples
 #' dat <- matrix(rnorm(10000), nrow=100, ncol=100)
 #' rownames(dat) <- paste("gene", 1:100, sep="")
@@ -73,175 +56,161 @@ NULL
 #' outcomes <- c(rep("A",50), rep("B",30), rep("C",20))
 #' names(outcomes) <- colnames(dat)
 #' 
-#' results <- calculate_emd(dat, outcomes, nperm=10, parallel=FALSE)
-#' head(results$emd)
+#' results <- calculate_cvm(dat, outcomes, nperm=10, parallel=FALSE)
+#' head(results$cvm)
 #' 
-#' @seealso \code{\link{EMDomics}} \code{\link[emdist]{emd2d}}
-calculate_emd <- function(data, outcomes, binSize=0.2,
-                            nperm=100, verbose=TRUE, parallel=TRUE) {
-
+#' @seealso \code{\link{EMDomics}} \code{\link[CDFt]{CramerVonMisesTwoSamples}}
+calculate_cvm <- function(data, outcomes,
+                          nperm=100, verbose=TRUE, parallel=TRUE) {
+  
   bpparam <- BiocParallel::bpparam()
-
+  
   if (parallel == FALSE)
     bpparam <- BiocParallel::SerialParam()
-
+  
   # transpose and coerce to df (for bplapply)
   data.df <- as.data.frame(t(data))
   sample_names <- rownames(data.df)
   
-  # ---------- pairwise emd table -----------
+  # ---------- pairwise cvm table -----------
   
-  # generate pairwise emd table for each gene
+  # generate pairwise cvm table for each gene
   if (verbose)
-    message("Calculating pairwise emd scores...", appendLF=FALSE)
+    message("Calculating pairwise cvm scores...", appendLF=FALSE)
   
   # all possible pairwise comparisons
   classes <- unique(outcomes)
   pairs <- combn(classes,2)
   names <- apply(pairs,2,function(x){paste(x[1],'vs',x[2])})
   
-  emd.tab <- BiocParallel::bplapply(data.df, .emd_pairwise_table, sample_names, 
-                                           outcomes, pairs,
-                                           binSize, verbose,
-                                           BPPARAM = bpparam)
+  cvm.tab <- BiocParallel::bplapply(data.df, .cvm_pairwise_table, sample_names, 
+                                    outcomes, pairs, verbose,
+                                    BPPARAM = bpparam)
   
-  # Remove genes that were not binned properly by the histogram
-  lengths <- lapply(emd.tab, function(x){length(x)})
-  remove.genes <- lengths < ncol(pairs)
-  bad.lengths <- lengths[remove.genes]
-  for (b in names(bad.lengths)) {
-    msg <- paste('Data for gene', b, 'has been removed because it could not be binned properly.')
-    message(msg)
-  }
-  emd.tab <- emd.tab[!remove.genes]
-  data.df <- data.df[,!remove.genes]
-  
-  emd.tab <- matrix(unlist(emd.tab), nrow=ncol(data.df), ncol=ncol(pairs), byrow=TRUE)
-  rownames(emd.tab) <- colnames(data.df)
-  colnames(emd.tab) <- names
+  cvm.tab <- matrix(unlist(cvm.tab), nrow=ncol(data.df), ncol=ncol(pairs), byrow=TRUE)
+  rownames(cvm.tab) <- colnames(data.df)
+  colnames(cvm.tab) <- names
   
   if (verbose)
     message("done.")
   
-  # ---------- emd ------------
-
-  # calculate emd for each gene
-  if (verbose)
-    message("Calculating emd...", appendLF=FALSE)
+  # ---------- cvm ------------
   
-  emd <- apply(emd.tab, 1, function(x){mean(as.numeric(x))})
-
-  emd <- as.matrix(emd)
-  colnames(emd) <- "emd"
-
+  # calculate cvm for each gene
+  if (verbose)
+    message("Calculating cvm...", appendLF=FALSE)
+  
+  cvm <- apply(cvm.tab, 1, function(x){mean(as.numeric(x))})
+  
+  cvm <- as.matrix(cvm)
+  colnames(cvm) <- "cvm"
+  
   if (verbose)
     message("done.")
-
-  # --------------- permuted emd scores ----------------
-
+  
+  # --------------- permuted cvm scores ----------------
+  
   sample_count <- length(outcomes)
-
-  # matrix to hold permuted emd values
-  emd.perm <- matrix(nrow=ncol(data.df), ncol=nperm)
-  rownames(emd.perm) <- colnames(data.df)
-  colnames(emd.perm) <- as.character(1:nperm)
-
+  
+  # matrix to hold permuted cvm values
+  cvm.perm <- matrix(nrow=ncol(data.df), ncol=nperm)
+  rownames(cvm.perm) <- colnames(data.df)
+  colnames(cvm.perm) <- as.character(1:nperm)
+  
   for (i in 1:nperm) {
-
-    msg <- paste("Calculating permuted emd #", i, " of ",
+    
+    msg <- paste("Calculating permuted cvm #", i, " of ",
                  nperm, "...", sep="")
-
+    
     if (verbose)
       message(msg, appendLF=FALSE)
-
+    
     # permute samples
     idx.perm <- sample(1:sample_count, replace=FALSE)
     sample.id <- names(outcomes)
     outcomes.perm <- outcomes[idx.perm]
     names(outcomes.perm) <- sample.id
-
-    # calculate emd for permuted samples
-    perm.val <- BiocParallel::bplapply(data.df, calculate_emd_gene,
-                                                   outcomes.perm, rownames(data.df),
-                                                   binSize,
-                                                   BPPARAM = bpparam)
-  
-    emd.perm[,i] <- unlist(sapply(perm.val,"[",1))
+    
+    # calculate cvm for permuted samples
+    perm.val <- BiocParallel::bplapply(data.df, calculate_cvm_gene,
+                                       outcomes.perm, rownames(data.df),
+                                       BPPARAM = bpparam)
+    
+    cvm.perm[,i] <- unlist(sapply(perm.val,"[",1))
     
     if (verbose)
       message("done.")
-
+    
   }
-
+  
   # ------------------ q-values --------------------
-
+  
   if (verbose)
     message("Calculating q-values...", appendLF=FALSE)
-
-  perm.medians <- apply(emd.perm,1,function(x){median(x)})
-
+  
+  perm.medians <- apply(cvm.perm,1,function(x){median(x)})
+  
   # generate thresholds and qval matrix
-  thr_upper <- ceiling(max(emd))
+  thr_upper <- ceiling(max(cvm))
   thr <- seq(thr_upper, 0, by = -0.001)
-  qvals <- matrix(1, nrow=nrow(emd), ncol=length(thr))
-
+  qvals <- matrix(1, nrow=nrow(cvm), ncol=length(thr))
+  
   colnames(qvals) <- thr
-  rownames(qvals) <- rownames(emd)
-
+  rownames(qvals) <- rownames(cvm)
+  
   # calculate fdr at each threshold
   j <- 0
   for (d in thr) {
-
+    
     j <- j+1
-
+    
     # calculate true discoveries at this threshold
-    idx <- which(emd > d)
+    idx <- which(cvm > d)
     n.signif <- length(idx)
-    genes.signif <- rownames(emd)[idx]
-
+    genes.signif <- rownames(cvm)[idx]
+    
     # calculate false discoveries at this threshold
     idx <- which(perm.medians > d)
     n.fd <- length(idx)
-
+    
     fdr <- n.fd/n.signif
     qvals[genes.signif, j] <- fdr
-
+    
   }
-
+  
   # final q-value = smallest fdr
-  emd.qval <- apply(qvals, 1, min)
-
-  emd.qval <- as.matrix(emd.qval)
-  colnames(emd.qval) <- "q-value"
-
+  cvm.qval <- apply(qvals, 1, min)
+  
+  cvm.qval <- as.matrix(cvm.qval)
+  colnames(cvm.qval) <- "q-value"
+  
   # leave q-values of 0 as-is
-
+  
   if (verbose)
     message("done.")
-
-  emd <- cbind(emd, emd.qval)
-
-  EMDomics(data, outcomes, emd, emd.perm, emd.tab)
-
+  
+  cvm <- cbind(cvm, cvm.qval)
+  
+  CVMomics(data, outcomes, cvm, cvm.perm, cvm.tab)
+  
 }
 
 
 #' @export
-#' @title Calculate EMD score for a single gene
+#' @title Calculate CVM score for a single gene
 #' @details All possible combinations of the classes are used as pairwise comparisons.
 #' The data in \code{vec} is divided based on class labels based on the \code{outcomes}
 #' identifiers given. For each pairwise computation, the \code{\link{hist}} function is
 #' used to generate histograms for the two groups. The densities are then retrieved
-#' and passed to  \code{\link[emdist]{emd2d}} to compute the pairwise EMD score. The 
-#' total EMD score for the given data is the average of the pairwise EMD scores.
+#' and passed to  \code{\link[cvmist]{cvm2d}} to compute the pairwise CVM score. The 
+#' total CVM score for the given data is the average of the pairwise CVM scores.
 #' 
 #' @param vec A named vector containing data (e.g. expression data) for a single
 #' gene.
 #' @param outcomes A vector of group labels for the samples. The names must correspond
 #' to the names of \code{vec}.
 #' @param sample_names A character vector with the names of the samples in \code{vec}.
-#' @param binSize The bin size to be used when generating histograms for each of the groups.
-#' @return The emd score is returned.
+#' @return The CVM score is returned.
 #' 
 #' @examples
 #' # 100 genes, 100 samples
@@ -253,17 +222,17 @@ calculate_emd <- function(data, outcomes, binSize=0.2,
 #' outcomes <- c(rep("A",50), rep("B",30), rep("C",20))
 #' names(outcomes) <- colnames(dat)
 #'
-#' calculate_emd_gene(dat[1,], outcomes, colnames(dat))
+#' calculate_cvm_gene(dat[1,], outcomes, colnames(dat))
 #' 
-#' @seealso \code{\link[emdist]{emd2d}}
-calculate_emd_gene <- function(vec, outcomes, sample_names, binSize=0.2) {
+#' @seealso \code{\link[CDFt]{CramerVonMisesTwoSamples}}
+calculate_cvm_gene <- function(vec, outcomes, sample_names) {
   
   names(vec) <- sample_names
   
   classes <- unique(outcomes)
   pairs <- combn(classes,2)
   
-  EMD.tab <- matrix(NA, nrow=1, ncol=dim(pairs)[2])
+  CVM.tab <- matrix(NA, nrow=1, ncol=dim(pairs)[2])
   colnames<-list()
   
   for (p in 1:dim(pairs)[2])
@@ -275,59 +244,59 @@ calculate_emd_gene <- function(vec, outcomes, sample_names, binSize=0.2) {
     src.lab <- names(outcomes[outcomes==src])
     sink.lab <- names(outcomes[outcomes==sink])
     
-    EMD <- .emd_gene_pairwise(vec,src.lab,sink.lab,binSize)
-    EMD.tab[1,p] <- EMD
+    CVM <- CDFt::CramerVonMisesTwoSamples(vec[src.lab],vec[sink.lab])
+    CVM.tab[1,p] <- CVM
   }
   
-  EMD.tab <- as.numeric(EMD.tab)
+  CVM.tab <- as.numeric(CVM.tab)
   
-  mean(EMD.tab)
+  mean(CVM.tab)
 }
 
 
 #' @export
-#' @title Create an EMDomics object
-#' @description This is the constructor for objects of class 'EMDomics'. It
-#' is used in \code{\link{calculate_emd}} to construct the return value.
+#' @title Create an CVMomics object
+#' @description This is the constructor for objects of class 'CVMomics'. It
+#' is used in \code{\link{calculate_cvm}} to construct the return value.
 #' 
 #' @param data A matrix containing genomics data (e.g. gene expression levels).
 #' The rownames should contain gene identifiers, while the column names should
 #' contain sample identifiers.
 #' @param outcomes A vector of group labels for each of the sample identifiers. The
 #' names of this vector must correspond to the column names of \code{data}.
-#' @param emd A matrix containing a row for each gene in \code{data}, and with
+#' @param cvm A matrix containing a row for each gene in \code{data}, and with
 #' the following columns:
 #' \itemize{
-#' \item \code{emd} The calculated emd score.
+#' \item \code{cvm} The calculated CVM score.
 #' \item \code{q-value} The calculated q-value.
 #' }
 #' The row names should specify the gene identifiers for each row.
-#' @param emd.perm A matrix containing a row for each gene in \code{data}, and
-#' with a column containing emd scores for each random permutation calculated
-#' via \code{\link{calculate_emd}}.
-#' @param pairwise.emd.table A table containing the EMD scores for each pairwise
+#' @param cvm.perm A matrix containing a row for each gene in \code{data}, and
+#' with a column containing CVM scores for each random permutation calculated
+#' via \code{\link{calculate_cvm}}.
+#' @param pairwise.cvm.table A table containing the CVM scores for each pairwise
 #' comparison for each gene. For a two-class problem, there should be only one column
 #' comparing class 1 and class 2. The row names should be gene identifiers. The column
 #' names should be in the format "<class 1> vs <class 2>" (e.g. "1 vs 2" or "A vs B").
 #' 
 #' @return The function combines its arguments in a list, which is assigned class
-#' 'EMDomics'. The resulting object is returned.
+#' 'CVMomics'. The resulting object is returned.
 #' 
-#' @seealso \code{\link{calculate_emd}}
-EMDomics <- function(data, outcomes, emd, emd.perm, pairwise.emd.table) {
-
+#' @seealso \code{\link{calculate_cvm}}
+CVMomics <- function(data, outcomes, cvm, cvm.perm, pairwise.cvm.table) {
+  
   structure(list("data"=data, "outcomes"=outcomes,
-                 "emd"=emd, "emd.perm"=emd.perm, "pairwise.emd.table"=pairwise.emd.table),
-            class = "EMDomics")
-
+                 "cvm"=cvm, "cvm.perm"=cvm.perm, "pairwise.cvm.table"=pairwise.cvm.table),
+            class = "CVMomics")
+  
 }
 
-# Creates a table of all the pairwise EMD scores for one gene
-.emd_pairwise_table <- function(geneData, sample_names, outcomes, pairs, binSize, verbose) {
+# Creates a table of all the pairwise CVM scores for one gene
+.cvm_pairwise_table <- function(geneData, sample_names, outcomes, pairs, verbose) {
   
   names(geneData) <- sample_names
   
-  EMD.tab <- matrix(NA, nrow=1, ncol=dim(pairs)[2])
+  CVM.tab <- matrix(NA, nrow=1, ncol=dim(pairs)[2])
   colnames<-list()
   
   for (p in 1:dim(pairs)[2])
@@ -339,32 +308,11 @@ EMDomics <- function(data, outcomes, emd, emd.perm, pairwise.emd.table) {
     src.lab <- names(outcomes[outcomes==src])
     sink.lab <- names(outcomes[outcomes==sink])
     
-    EMD <- .emd_gene_pairwise(geneData,src.lab,sink.lab,binSize)
-    EMD.tab[1,p] <- EMD
+    CVM <- CDFt::CramerVonMisesTwoSamples(geneData[src.lab],geneData[sink.lab])
+    CVM.tab[1,p] <- CVM
   }
-
-  EMD.tab <- as.numeric(EMD.tab)
   
-  EMD.tab
-}
-
-# computes pairwise EMD
-.emd_gene_pairwise <- function(vec, idxA, idxB, binSize=0.2) {
-  dataA <- vec[idxA]
-  dataB <- vec[idxB]
+  CVM.tab <- as.numeric(CVM.tab)
   
-  dataA <- as.numeric(dataA)
-  dataB <- as.numeric(dataB)
-  
-  bins <- seq(floor(min(c(dataA, dataB))),
-              ceiling(max(c(dataA, dataB))),
-              by=binSize )
-  
-  histA <- hist(dataA, breaks=bins, plot=FALSE)
-  histB <- hist(dataB, breaks=bins, plot=FALSE)
-  
-  densA <- as.matrix(histA$density)
-  densB <- as.matrix(histB$density)
-  
-  emdist::emd2d(densA, densB)
+  CVM.tab
 }
